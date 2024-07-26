d975ace7ea02d449b519b4f92b0b226304db5b96
Sandra Palacios <sandraanabel21@gmail.com>
2024-07-26 16:36:47 -0500

    creation of the hotel.model.ts

---
+++ src/app/utilitarian/models/hotel.model.ts
@@ -0,0 +1,8 @@
+export interface Hotel{
+  name: string;
+  rating: number;
+  weekdayRateRegular: number;
+  weekdayRateRewards: number;
+  weekendRateRegular: number;
+  weekendRateRewards: number;
+}
